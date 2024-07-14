b994a2092160e150d3c8027320aef455da3c0dd2
Konrad Jamrozik <kojamroz@microsoft.com>
2024-07-14 15:11:25 -0700

    tslint-eslint config

--- eng/tools/specs-model/eslint.config.js
+++ eng/tools/specs-model/eslint.config.js
@@ -1,11 +1,37 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
+// @ts-check
+
+// This file contents is based on:
+// https://typescript-eslint.io/getting-started#step-2-configuration
+// https://typescript-eslint.io/getting-started/typed-linting/#shared-configurations
+
+import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-]
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+)
+
+// export default [
+//   { files: ['**/*.{js,mjs,cjs,ts}'] },
+//   { languageOptions: { globals: globals.browser } },
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommendedTypeChecked,
+//   ...tseslint.configs.stylisticTypeChecked,
+// ]
+
+// export default tseslint.config(
+//   eslint.configs.recommended,
+//   ...tseslint.configs.recommendedTypeChecked,
+//   ...tseslint.configs.stylisticTypeChecked,
+// );
 
