1b2b7280d578005a99cd1646339c3e4bb3fdf2d9
Konrad Jamrozik <kojamroz@microsoft.com>
2024-07-14 16:01:09 -0700

    better eslint config

--- eng/tools/specs-model/eslint.config.js
+++ eng/tools/specs-model/eslint.config.js
@@ -4,12 +4,10 @@
 // https://typescript-eslint.io/getting-started#step-2-configuration
 // https://typescript-eslint.io/getting-started/typed-linting/#shared-configurations
 
-import globals from 'globals'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
-export default tseslint.config(
+const config = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
@@ -19,22 +17,28 @@ export default tseslint.config(
         project: true,
         tsconfigRootDir: import.meta.dirname,
       },
-      globals: globals.builtin,
     },
-    plugins: {
-      unicorn: eslintPluginUnicorn,
-    },
-    ignores: ['dist', 'eslint.config.js', '.prettierrc.cjs'],
     rules: {
       // Sometimes we have to help the type checker with "!":
       // e.g. when doing `if (arr.length > 0) { const ... = arr[0]! }`
       // https://typescript-eslint.io/rules/no-non-null-assertion
       // Note: this originates from [strict]
       '@typescript-eslint/no-non-null-assertion': 'off',
-      'unicorn/no-instanceof-array': 'error',
     },
+  },
+  {
+    // disable type-aware linting on JS files
+    // Config snippet taken from https://typescript-eslint.io/packages/typescript-eslint/#advanced-usage
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
   }
 )
+
+export default config
+
+// Debug tool:
+// Uncomment to print the config. View it in VS Code / Output / ESLint. Run "ESLint: Restart ESLint Server" command to force output.
+// console.log(`ESLint config: ${JSON.stringify(config)}`)
 
 // export default [
 //   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -43,12 +47,6 @@ export default tseslint.config(
 //   ...tseslint.configs.recommendedTypeChecked,
 //   ...tseslint.configs.stylisticTypeChecked,
 // ]
-
-// export default tseslint.config(
-//   eslint.configs.recommended,
-//   ...tseslint.configs.recommendedTypeChecked,
-//   ...tseslint.configs.stylisticTypeChecked,
-// );
 
 // [strict]: https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/strict.ts
 
